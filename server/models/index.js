7e689f05923e17f8faa0ced8b35faa4cb6680ff7
Utif Milkedori <utifmd@gmail.com>
2023-01-14 15:38:15 +0700

    deployment with assigned module

--- server/models/index.js
+++ server/models/index.js
@@ -4,12 +4,14 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
+const postgres = require("pg");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
+config.dialectModule = postgres;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
